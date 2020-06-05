1de5f50be863be4dae561e9ffbf4f7772511083c
hamishgray <hamish.gray@secretescapes.com>
2020-06-05 11:14:28 +0100

    fromApp condition. Fix styling to be same on UK & DE

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,6 @@
 // general js for the project that wouldn't be a reuseable component
+
+var modalQueryString = 'fromApp';
+if (getQueryStringByName(modalQueryString)) {
+  $('body').addClass('fromApp');
+}
